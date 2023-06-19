ea6295f5551572eb1306501cef83ed6b2be118d7
Tamás <tmk.m90@gmail.com>
2023-06-19 12:13:37 +0200

    modal-window

---
+++ script.js
@@ -0,0 +1,30 @@
+// Modal window
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.btn--close-modal')
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+
+const openModal = (e) => {
+  e.preventDefault() // don't jump to page top
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
+// for (let i = 0; i < btnsOpenModal.length; i++)
+// btnsOpenModal[i].addEventListener('click', openModal)
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
